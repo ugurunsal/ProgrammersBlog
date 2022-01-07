eb4f08b4096887d4572575b6984ae142e8dca9de
Uğur Ünsal <ugur1905unsal@gmail.com>
2022-01-07 18:45:22 +0300

    Hakkında ve İletişim sayfaları eklendi.

--- ProgrammersBlog.Mvc/Controllers/HomeController.cs
+++ ProgrammersBlog.Mvc/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Entities.Dtos;
 using ProgrammersBlog.Services.Abstract;
 using System;
 using System.Collections.Generic;
@@ -10,10 +13,12 @@ namespace ProgrammersBlog.Mvc.Controllers
     public class HomeController : Controller
     {
         private readonly IArticleService _articleService;
+        private readonly AboutUsPageInfo _aboutUsPageInfo;
 
-        public HomeController(IArticleService articleService)
+        public HomeController(IArticleService articleService, IOptions<AboutUsPageInfo> aboutUsPageInfo)
         {
             _articleService = articleService;
+            _aboutUsPageInfo = aboutUsPageInfo.Value;
         }
 
         [HttpGet]
@@ -23,6 +28,24 @@ namespace ProgrammersBlog.Mvc.Controllers
                 ? _articleService.GetAllByPagingAsync(null, currentPage, pageSize,isAscending)
                 : _articleService.GetAllByPagingAsync(categoryId.Value, currentPage, pageSize,isAscending));
             return View(articlesResult.Data);
+        }
+
+        [HttpGet]
+        public IActionResult About()
+        {
+            return View(_aboutUsPageInfo);
+        }
+
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(EmailSendDto emailSendDto)
+        {
+            return View();
         }
     }
 }
--- ProgrammersBlog.Mvc/Startup.cs
+++ ProgrammersBlog.Mvc/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProgrammersBlog.Entities.Concrete;
 using ProgrammersBlog.Mvc.AutoMapper.Profiles;
 using ProgrammersBlog.Mvc.Helpers.Abstract;
 using ProgrammersBlog.Mvc.Helpers.Concrete;
@@ -30,6 +31,8 @@ namespace ProgrammersBlog.Mvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<AboutUsPageInfo>(Configuration.GetSection("AboutUsPageInfo"));
+            services.Configure<WebsiteInfo>(Configuration.GetSection("WebsiteInfo"));
             services.AddRazorPages();
             services.AddControllersWithViews(options=>
             {
